145efb4671d905fab204ebb679753cf3e8257662
Molnár Péter <ranlom0503@gmail.com>
2026-01-08 06:34:36 +0100

    Board class refactored

--- src/main/java/org/example/domain/Board.java
+++ src/main/java/org/example/domain/Board.java
@@ -2,25 +2,30 @@ package org.example.domain;
 
 public class Board {
 
-    private final int size;
     private final Symbol[][] fields;
 
     public Board(int size) {
-        this.size = size;
-        this.fields = new Symbol[size][size];
+        fields = new Symbol[size][size];
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                fields[row][col] = Symbol.EMPTY;
+            }
+        }
     }
 
     public int getSize() {
-        return size;
+        return fields.length;
     }
 
-    public Symbol get(int row, int col){
+    public Symbol get(int row, int col) {
         return fields[row][col];
     }
 
-    public void set(int row, int col, Symbol symbol){
+    public boolean placeSymbol(int row, int col, Symbol symbol) {
+        if (fields[row][col] != Symbol.EMPTY) {
+            return false;
+        }
         fields[row][col] = symbol;
+        return true;
     }
-
-
 }
