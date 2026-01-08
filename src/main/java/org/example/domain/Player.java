0d6d7cc05b0b84c8c8a8c400ce2822e9ce98826e
Molnár Péter <ranlom0503@gmail.com>
2026-01-08 06:37:24 +0100

    domani - Player class refactored

--- src/main/java/org/example/domain/Player.java
+++ src/main/java/org/example/domain/Player.java
@@ -5,10 +5,19 @@ package org.example.domain;
 public abstract class Player {
     private int row;
     private int col;
+    private final Symbol symbol;
+
+    protected Player(Symbol symbol){
+        this.symbol = symbol;
+    }
 
     public void setPosition(int row, int col) {
         this.row = row;
         this.col = col;
+    }
+
+    public Symbol getSymbol() {
+        return symbol;
     }
 
     public int getRow() {
